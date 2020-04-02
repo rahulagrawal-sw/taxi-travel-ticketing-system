97f69c6be8d38c53e3bf8519e3dab58b3bfe9caa
rahul <rahulagrawal.sw@gmail.com>
2020-04-02 12:37:57 +0530

    Story1: Main method added to run application to showcase end to end processing

--- src/main/java/com/rna/TaxiTravelTicketingSystem.java
+++ src/main/java/com/rna/TaxiTravelTicketingSystem.java
@@ -1,4 +1,18 @@
 package com.rna;
 
+import com.rna.dto.TravelRequest;
+import com.rna.dto.TravelResponse;
+import com.rna.service.TravelFareCalculator;
+import com.rna.service.TravelFareCalculatorService;
+import com.rna.util.TicketPrintingUtil;
+
 public class TaxiTravelTicketingSystem {
+
+    public static void main(String[] args) {
+        TravelFareCalculator travelFareCalculator = new TravelFareCalculatorService();
+        TravelRequest travelRequest = new TravelRequest("PUNE", "NASHIK", 1);
+        TravelResponse travelResponse = travelFareCalculator.calculateFare(travelRequest);
+
+        TicketPrintingUtil.printTicket(travelRequest, travelResponse);
+    }
 }
